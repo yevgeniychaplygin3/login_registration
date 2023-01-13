10850b8a1d658d141db1d525d3e54f2bfca11036
yevgeniychaplygin3 <ychaplygin@yahoo.com>
2023-01-13 11:31:27 -0800

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using login_registration.Models;
+
+namespace login_registration.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<login_registration.Models.Users> Users { get; set; }
+    }
+}
---
+++ Models/Users.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+namespace login_registration.Models
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        public Users()
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        { 
+        
+        }
+    }
+}
